d5ed10559ca3f98502853b7010a7435136590570
DanniPilla <daniellapilla08@gmail.com>
2024-07-31 21:12:01 +1000

    Lab Exercise 4

---
+++ Backend/helloworld.js
@@ -0,0 +1,39 @@
+console.log('Hello Danni')
+//Task1
+
+//Addition
+function additionFunction(a, b) {
+  return a + b;
+}
+console.log(additionFunction(5, 1));
+
+//subtraction
+function minusFunction(a, b) {
+  return a - b;
+}
+
+console.log(minusFunction(5, 1));
+
+//division
+function divideFunction(a, b) {
+  return a / b;
+}
+
+console.log(divideFunction(5, 1));
+
+//multiply
+function multiplyFunction(a, b) {
+  return a * b;
+}
+console.log(multiplyFunction(5, 2));
+
+//Task2
+let text1 = "Hello";
+let text2 = "Danni";
+
+function helloDanni(text1, text2) {
+    return text1.concat(text2);
+}
+
+
+console.log(helloDanni);
