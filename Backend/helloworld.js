35a04626bb45e4036f966c6dcd2244113b225cce
DanniPilla <daniellapilla08@gmail.com>
2024-08-03 12:19:34 +1000

    Lesson 5 updates 3/08/2024

--- Backend/helloworld.js
+++ Backend/helloworld.js
@@ -33,3 +33,17 @@ function helloDanni(a, b) {
 }
 
 console.log("Hello", " Danni");
+
+//Class Exercise NPM 
+const Sentiment = require('sentiment');
+const sentiment = new Sentiment();
+
+function getSentiment(sentence) {
+const result = sentiment.analyze('Cats are actually perfect, there is a reason they were seen as gods');
+console.log(result); 
+}
+//Debugging class exercise
+console.log('this is a Message')
+console.info('this is info')
+console.warn('this is a warning')
+console.error('this is an error');
